b1a488a30db0fd4d7209bfaa5a4670d310fca56e
Marcelo-Alvess <marcelo.alves.sousa@outlook.com>
2021-06-25 18:31:46 -0300

    Projeto NLWValoriza completo

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,40 @@
+import { getCustomRepository } from "typeorm";
+import { UsersRepositories } from "../repositories/UsersRepositories";
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+
+interface IAuthenticateRequest {
+    email: string;
+    password: string;
+}
+
+export class AuthenticateUserService {
+    
+    
+    async execute({ email, password }: IAuthenticateRequest) {
+        const usersRepositories = getCustomRepository(UsersRepositories);
+
+        const user = await usersRepositories.findOne({
+            email
+        });
+
+        if(!user) {
+            throw new Error("Email/Password incorrect");
+        }
+
+        const passwordMatch = await compare(password, user.password);
+
+        if(!passwordMatch) {
+            throw new Error("Email/Password incorrect");
+        }
+
+        const token = sign({
+            emai: user.email
+        }, "ff3e804d698c54d98c6c8aac66b5fc72", {
+            subject: user.id,
+            expiresIn: "1d"
+        })
+
+        return token;
+    }
+}
